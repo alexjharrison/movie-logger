3394e847a85428247a079f7cbfe683c91e5fc34b
Alex Harrison <alexjharrison@gmail.com>
2018-12-14 20:21:35 -0500

    made vue stuff

---
+++ client/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <button @click="clickHandler">I'm a button</button>
+    <h1>{{ couter }}</h1>
+    <AntiButton
+      v-on:decrement="decrementHandler"
+      v-for="decrementAmount in decrementAmounts"
+      v-bind:amount="decrementAmount"
+      v-bind:key="decrementAmount"
+    />
+    <input type="text" v-model="text" name="thing" id="blah">
+  </div>
+</template>
+
+<script>
+import AntiButton from "./components/AntiButton.vue";
+
+export default {
+  components: {
+    AntiButton
+  },
+  mounted() {
+    console.log("value: " + this.text);
+  },
+  data() {
+    return {
+      couter: 0,
+      decrementAmounts: [10, 100, 1000, 10000],
+      text: "3"
+    };
+  },
+  methods: {
+    clickHandler() {
+      this.couter++;
+    },
+    decrementHandler(amount) {
+      this.couter -= amount;
+    }
+  },
+  watch: {
+    text() {
+      console.log("value: " + this.text);
+    }
+  }
+};
+</script>
+
+<style>
+button {
+}
+</style>
---
+++ client/src/components/AntiButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="AntiButton">
+    <button @click="decrementHandler2">Don't click me</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["amount"],
+  methods: {
+    decrementHandler2() {
+      this.$emit("decrement", this.amount);
+    }
+  }
+};
+</script>
+
+<style>
+#AntiButton button {
+  width: 500px;
+}
+</style>
