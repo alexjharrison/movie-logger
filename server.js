570a4da0b93e3efc109cae0aac157fe78d27a3d3
Alexander Harrison <aharrison@stifirestop.com>
2018-12-17 15:12:42 -0500

    process.env.PORT instead of port

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use("/api", require("./routes/api"));
 app.use(express.static("./client/dist"));
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, err => {
   if (err) console.log(err);
